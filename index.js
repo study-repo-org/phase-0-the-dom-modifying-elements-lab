fa9112c7635a3bc523202d0c909b46fe11d39e0a
Mohamed Ahmed <ma07041705@gmail.com>
2024-03-07 07:57:46 +0300

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+// Created <h1> element
+const newHeader = document.createElement('h1');
+
+//and  Set the element with an id of  'victory'
+newHeader.id = 'victory';
+
+// last added a text to the <h1> element
+newHeader.textContent = "YOUR-NAME is the champion"; 
